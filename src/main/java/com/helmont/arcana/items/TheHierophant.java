4b22ac60089ca06e1c154650d6cf071c54af8427
CrevanHelmont <ArcCrevanHelmont@gmail.com>
2022-07-06 23:19:35 -0500

    Hierophant & Dynamic tooltips

--- src/main/java/com/helmont/arcana/items/TheHierophant.java
+++ src/main/java/com/helmont/arcana/items/TheHierophant.java
@@ -1,22 +1,19 @@
 package com.helmont.arcana.items;
 
-import com.helmont.arcana.util.MiscUtil;
 import net.minecraft.client.item.TooltipContext;
-import net.minecraft.enchantment.MendingEnchantment;
 import net.minecraft.entity.Entity;
-import net.minecraft.entity.EntityType;
 import net.minecraft.entity.player.PlayerEntity;
 import net.minecraft.item.Item;
 import net.minecraft.item.ItemStack;
 import net.minecraft.nbt.NbtCompound;
-import net.minecraft.text.*;
+import net.minecraft.text.MutableText;
+import net.minecraft.text.Text;
 import net.minecraft.util.Formatting;
 import net.minecraft.util.Hand;
 import net.minecraft.util.TypedActionResult;
 import net.minecraft.world.World;
 
 import java.util.List;
-import java.util.Optional;
 
 public class TheHierophant extends Item {
     public TheHierophant(Settings settings) {
@@ -24,16 +21,12 @@ public class TheHierophant extends Item {
     }
 
     MutableText experience = Text.literal("");
-    MutableText levels = Text.literal("");
     MutableText mode = Text.literal("");
 
-    String[] modes = new String[]{"Drain", "Give"};
-
+    public static String[] modes = new String[]{"Drain", "Give"};
 
     @Override
     public void appendTooltip(ItemStack itemStack, World world, List<Text> tooltip, TooltipContext tooltipContext){
-        tooltip.add(this.experience.formatted(Formatting.GREEN));
-        tooltip.add(this.mode.formatted(Formatting.GREEN));
         tooltip.add(Text.translatable("item.arcproj.the_hierophant.tooltip").formatted(Formatting.GOLD));
     }
 
@@ -58,7 +51,6 @@ public class TheHierophant extends Item {
 
         int cardExp = hierophantNbt.getInt("TotalExp");
         int currentMode = hierophantNbt.getInt("Mode");
-
         if (currentMode == 0 && entity instanceof PlayerEntity player && player.totalExperience >= 1 && cardExp < 10000) {
 
             if (player.totalExperience >= 5) {
@@ -80,9 +72,11 @@ public class TheHierophant extends Item {
         }
 
         stack.setNbt(hierophantNbt);
+        if (selected) {
+            this.experience = Text.literal("Total Experience: " + hierophantNbt.getInt("TotalExp") + "/10000");
+            this.mode = Text.literal("Mode: ").append(Text.literal(modes[hierophantNbt.getInt("Mode")]).formatted(Formatting.BLUE));
 
-        this.experience = Text.literal("Total Experience: " + hierophantNbt.getInt("TotalExp") + "/10000");
-        this.mode = Text.literal("Mode: ").append(Text.literal(modes[hierophantNbt.getInt("Mode")]).formatted(Formatting.BLUE));
+        }
     }
 
     @Override
--- src/main/java/com/helmont/arcana/util/ModRegistries.java
+++ src/main/java/com/helmont/arcana/util/ModRegistries.java
@@ -1,33 +1,28 @@
 package com.helmont.arcana.util;
 
 import com.helmont.arcana.Arcana;
+import com.helmont.arcana.items.TheHierophant;
 import com.helmont.arcana.registry.ModItems;
 import com.helmont.arcana.status_effects.FooledEffect;
 import com.helmont.arcana.villager.ArcanaVillagers;
-import com.mojang.brigadier.Message;
+import net.fabricmc.fabric.api.client.item.v1.ItemTooltipCallback;
 import net.fabricmc.fabric.api.object.builder.v1.trade.TradeOfferHelper;
-import net.fabricmc.loader.api.FabricLoader;
-import net.minecraft.client.MinecraftClient;
 import net.minecraft.entity.effect.StatusEffect;
-import net.minecraft.entity.passive.SheepEntity;
 import net.minecraft.item.ItemStack;
 import net.minecraft.item.Items;
-import net.minecraft.scoreboard.Scoreboard;
-import net.minecraft.scoreboard.ScoreboardCriterion;
-import net.minecraft.server.MinecraftServer;
-import net.minecraft.server.command.GiveCommand;
+import net.minecraft.nbt.NbtCompound;
 import net.minecraft.text.Text;
-import net.minecraft.text.Texts;
+import net.minecraft.util.Formatting;
 import net.minecraft.util.Identifier;
 import net.minecraft.util.registry.Registry;
 import net.minecraft.village.TradeOffer;
-import net.minecraft.village.VillagerProfession;
 
 public class ModRegistries {
 
     public static void registerModStuff() {
         registerCustomTrades();
         registerStatusEffects();
+        tooltipadd();
     }
 
     public static void registerCustomTrades() {
@@ -38,6 +33,22 @@ public class ModRegistries {
                             new ItemStack(ModItems.TAROT_CARD, 1),
                             10,2,0.02f));
                 });
+    }
+
+    public static void tooltipadd() {
+        ItemTooltipCallback.EVENT.register((stack, context, lines) -> {
+            if (stack.getItem() == ModItems.THE_HIEROPHANT) {
+                NbtCompound nbtData = stack.getNbt();
+                if (nbtData != null) {
+                    lines.add(1, Text.literal("Total EXP: " + nbtData.getInt("TotalExp") + "/10000")
+                            .formatted(Formatting.GREEN));
+                    lines.add(2, Text.literal("Mode: ")
+                            .formatted(Formatting.GREEN)
+                            .append(Text.literal(TheHierophant.modes[nbtData.getInt("Mode")])
+                                    .formatted(Formatting.BLUE)));
+                }
+            }
+        });
     }
 
     public static final StatusEffect FOOLED = new FooledEffect();
